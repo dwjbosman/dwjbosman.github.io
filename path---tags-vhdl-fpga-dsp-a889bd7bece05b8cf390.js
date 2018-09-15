deb550fd6d7927eb5becf0f50c539f3ae0a6d812
Dinne Bosman <dwjbosman@yahoo.com>
2018-09-15 22:53:15 +0200

    publish

---
+++ path---tags-vhdl-fpga-dsp-a889bd7bece05b8cf390.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x73a07d970a50],{466:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{fields:{slug:"/vhdl-i-2-s-transmitter"},excerpt:'This summer I have been reading a book on VHDL 2008: "Effective Coding with VHDL" from Ricardo Jasinski. Although not for beginners I can…',timeToRead:8,frontmatter:{title:"VHDL i2s transmitter",tags:["VHDL FPGA DSP"],cover:"/logos/wave.jpg",date:"2018-09-14 22:00"}}}]}},pathContext:{tag:"VHDL FPGA DSP"}}}});
+//# sourceMappingURL=path---tags-vhdl-fpga-dsp-a889bd7bece05b8cf390.js.map
